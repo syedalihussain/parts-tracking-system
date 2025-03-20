45ab09277c4bb982d3f8a584032b2a4a37e76c20
syedalihussain <ali.hussain@healthdirect.org.au>
2025-03-20 20:38:50 +1100

    feat(ui): finished creating a revenue chart

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,5 +1,5 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
-// import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 // import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from '@/app/ui/fonts';
 // import { Suspense } from "react";
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -19,3 +19,33 @@ export async function fetchCardData() {
     throw new Error('Failed to fetch card data.');
   }
 }
+
+export async function fetchRevenue() {
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const data = [
+      { month: 'Jan', revenue: 2000 },
+      { month: 'Mar', revenue: 2200 },
+      { month: 'Feb', revenue: 1800 },
+      { month: 'Jul', revenue: 3500 },
+      { month: 'Aug', revenue: 3700 },
+      { month: 'Oct', revenue: 2800 },
+      { month: 'Nov', revenue: 3000 },
+      { month: 'Sep', revenue: 2500 },
+      { month: 'Dec', revenue: 4800 },
+      { month: 'Apr', revenue: 2500 },
+      { month: 'Jun', revenue: 3200 },
+      { month: 'May', revenue: 2300 },
+    ];
+
+    return data;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch revenue data.');
+  }
+}
