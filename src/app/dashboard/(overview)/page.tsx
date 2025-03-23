33b9f96882a78dc0919ec7f09c3b7f8afc196f2f
syedalihussain <hussain.syedalishah@gmail.com>
2025-03-23 13:05:45 +1100

    feat(ui): basic latest-invoices component

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,6 +1,6 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
-// import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-// import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 // import { Suspense } from "react";
 // import {
@@ -17,6 +17,10 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <CardWrapper />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart />
+        <LatestInvoices />
       </div>
     </main>
   );
---
+++ src/app/ui/dashboard/latest-invoices.tsx
@@ -0,0 +1,20 @@
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '../fonts';
+
+export default async function LatestInvoices() {
+  return (
+    <div className="w-full md:col-span-4">
+      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Latest Invoices
+      </h2>
+
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4"></div>
+        <div className="flex items-center pb-2 pt-6">
+          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
